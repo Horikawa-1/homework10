2e0e373d1df5a3e9ba5af5e9335327f0cc01131a
Horikawa-1 <111167638+Horikawa-1@users.noreply.github.com>
2023-03-19 12:44:39 +0900

    Update CreateForm.java

--- src/main/java/com/raisetech/homework9/entity/CreateForm.java
+++ src/main/java/com/raisetech/homework9/entity/CreateForm.java
@@ -3,15 +3,14 @@ package com.raisetech.homework9.entity;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
+
 
 
 public class CreateForm {
 
 
-  public CreateForm(String name, int id) {
+  public CreateForm(String name) {
     this.name = name;
-    this.id = id;
   }
 
   public String getName() {
@@ -22,6 +21,4 @@ public class CreateForm {
   @Length(max = 20)
   private final String name;
 
-  @Positive//数値が正でないとエラー
-  private final int id;
 }
