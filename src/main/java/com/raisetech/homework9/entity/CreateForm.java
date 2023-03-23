c91df1de2e087d12fb95761eda235d5f7ddbf3be
Horikawa-1 <111167638+Horikawa-1@users.noreply.github.com>
2023-03-23 22:49:44 +0900

    Update CreateForm.java

--- src/main/java/com/raisetech/homework9/entity/CreateForm.java
+++ src/main/java/com/raisetech/homework9/entity/CreateForm.java
@@ -1,21 +1,22 @@
 package com.raisetech.homework9.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.NotBlank;
 
-
-
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class CreateForm {
 
-
-  public CreateForm(String name) {
-    this.name = name;
-  }
 
   public String getName() {
     return name;
   }
+
 
   @NotBlank
   @Length(max = 20)
